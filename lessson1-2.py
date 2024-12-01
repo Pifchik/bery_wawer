a19da9a05a2a606bdda4189dade48c8a2a8143c5
Pifchik <153438541+Pifchik@users.noreply.github.com>
2024-12-01 16:43:16 +0200

    Add files via upload

---
+++ lessson1-2.py
@@ -0,0 +1,151 @@
+# from ctypes.wintypes import PLARGE_INTEGER
+
+
+# print("Hello world")
+# #тут ізі в нас цілі ціфри int
+# number = 10
+# #це щось типу не ціли цифри Float
+# price = 9.99
+# #Кароче слово String це шомсь типу текст
+# name =  "Tymofii"
+# #Ну тут зрозуміло це bool
+# light = True
+
+# chifric1 = 11
+# chifric2 =  13
+# Ne_povni_chifric = 12.9
+# Ne_povni_chifric2 = 12.1
+# ZBaty_Yak  = "Tymofii"
+# ZBaty_Yak2 = "Motya"
+
+# print(type(chifric1))
+# print(type(Ne_povni_chifric))
+# print(type(ZBaty_Yak))
+
+
+# tymofii = 5
+# matvii  = 4
+
+# if tymofii < 6:
+#  print("2 points")
+# elif matvii <  6:
+#      print("2 points")
+# else:
+#     print("boys have 12 points")
+
+
+# player_1 = 4
+# player_2 = 1
+ 
+# if player_1  > 3:
+#     print("You are a lose :(")
+# elif  player_2  > 3:
+#   print("You are a lose  :(")
+# elif player_1  < 6:
+#     print("You are a win :)")
+# elif player_2  < 6:
+#         print("You are a win :)")
+# else: 
+#     print("You win:)")
+
+# from ctypes.wintypes import PLARGE_INTEGER
+
+
+# print("Hello world")
+# #тут ізі в нас цілі ціфри int
+# number = 10
+# #це щось типу не ціли цифри Float
+# price = 9.99
+# #Кароче слово String це шомсь типу текст
+# name =  "Tymofii"
+# #Ну тут зрозуміло це bool
+# light = True
+
+# chifric1 = 11
+# chifric2 =  13
+# Ne_povni_chifric = 12.9
+# Ne_povni_chifric2 = 12.1
+# ZBaty_Yak  = "Tymofii"
+# ZBaty_Yak2 = "Motya"
+
+# print(type(chifric1))
+# print(type(Ne_povni_chifric))
+# print(type(ZBaty_Yak))
+
+
+# tymofii = 5
+# matvii  = 4
+
+# if tymofii < 6:
+#  print("2 points")
+# elif matvii <  6:
+#      print("2 points")
+# else:
+#     print("boys have 12 points")
+
+
+# player_1 = 4
+# player_2 = 1
+ 
+# if player_1  > 3:
+#     print("You are a lose :(")
+# elif  player_2  > 3:
+#   print("You are a lose  :(")
+# elif player_1  < 6:
+#     print("You are a win :)")
+# elif player_2  < 6:
+#         print("You are a win :)")
+# else: 
+#     print("You win:)")
+
+
+
+
+
+
+
+
+# for i in range(50):
+#      print("Hello world")
+# a = 5
+# while a < 10:
+#    a+= 1
+# print(a)
+
+
+# for i in range(50):
+#     print("Hello  wawawawa")
+
+# Bukva_D = 5
+# while a < 10:
+#    Bukva_D+= 1
+# print(Bukva_D)
+
+a = 10
+Bukva_D =  12
+
+if a  < 11:
+    print("Not today :(")
+elif  Bukva_D < 12:
+ for i in range(50):
+    print("Hello  wawawawa")
+
+Bukva_D = 5
+while a < 10:
+   Bukva_D+= 1
+print(Bukva_D)
+
+else: a > 10:
+for i in range(50):
+ print("Hello world")
+a = 5
+while a < 10:
+   a+= 1
+print(a)
+
+
+ 
+        
+ 
+    
+
