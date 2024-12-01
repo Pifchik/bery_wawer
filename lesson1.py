c1af99aa1a0e7b9255a8f907fcd6e8729835b8f9
Pifchik <153438541+Pifchik@users.noreply.github.com>
2024-12-01 16:42:36 +0200

    Add files via upload

---
+++ lesson1.py
@@ -0,0 +1,10 @@
+name = "Tymofii"
+surname = "Dedekayev"
+How_Many_Year_Old  =  12
+How_Many_Year_Old_In_this_Years_to_Be = 13
+Now_you_have_12_years = True
+Now_you_have_13_years = False
+print(type(Now_you_have_12_years))
+print(type(Now_you_have_13_years))
+print(type(name))
+print(type(How_Many_Year_Old))
